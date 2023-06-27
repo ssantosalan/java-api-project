a31b35ab7737d9119dce846aedd818ec2167c1d6
Alan Santos Everymind <alan.santos@everymind.com.br>
2023-06-27 20:24:35 -0300

    #6 Criar login - classe Pessoa

---
+++ back-end/poke-project/src/main/java/com/api/pokeproject/Entity/Pessoa.java
@@ -0,0 +1,72 @@
+package com.api.pokeproject.Entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PESSOA")
+public class Pessoa implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, length = 70)
+    private String name;
+    @Column(nullable = false, unique = true, length = 70)
+    private String email;
+    @Column(nullable = false, unique = true, length = 50)
+    private String password;
+    @Column(nullable = false, unique = true, length = 50)
+    private String CPF;
+
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCPF() {
+        return this.CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+
+}
