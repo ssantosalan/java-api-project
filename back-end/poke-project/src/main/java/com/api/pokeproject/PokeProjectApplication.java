eadb21c57a844670d7924197f5c6e7c99bb51bd9
Alan Santos Everymind <alan.santos@everymind.com.br>
2023-06-26 22:01:32 -0300

    estrutura inicial back

---
+++ back-end/poke-project/src/main/java/com/api/pokeproject/PokeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.api.pokeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeProjectApplication.class, args);
+	}
+
+}
---
+++ back-end/poke-project/src/test/java/com/api/pokeproject/PokeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pokeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
